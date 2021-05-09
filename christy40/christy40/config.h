71a6ddfd2d864ff2b80bc9610ef7f44aee891322
Muhammad Rivaldi <50755960+valdiieee@users.noreply.github.com>
2021-05-09 08:34:28 +0700

    Add files via upload

---
+++ christy40/christy40/config.h
@@ -0,0 +1,47 @@
+/* Copyright 2021 REPLACE_WITH_YOUR_NAME
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0xAB69
+#define PRODUCT_ID      0x8686
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    valdydesu_
+#define PRODUCT         christy40
+#define DESCRIPTION     40% mechanical keyboard
+
+/* key matrix size */
+#define MATRIX_ROWS 4
+#define MATRIX_COLS 12
+
+/*
+ * Keyboard Matrix Assignments
+ *
+ * Change this to how you wired your keyboard
+ * COLS: AVR pins used for columns, left to right
+ * ROWS: AVR pins used for rows, top to bottom
+ * DIODE_DIRECTION: COL2ROW = COL = Anode (+), ROW = Cathode (-, marked on diode)
+ *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
+ *
+ */
+#define MATRIX_ROW_PINS { B1, B3, B2, B6 }
+#define MATRIX_COL_PINS { F4, F5, F6, F7, D1, D0, D4, C6, D7, E6, B4, B5 }
+
+#define DIODE_DIRECTION COL2ROW
+
