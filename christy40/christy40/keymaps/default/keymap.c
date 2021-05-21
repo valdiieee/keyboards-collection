8918635007d4fd46440912c4c90385d469a52ddd
Muhammad Rivaldi <50755960+valdiieee@users.noreply.github.com>
2021-05-21 16:41:05 +0700

    Update keymap.c
    add rgb control

--- christy40/christy40/keymaps/default/keymap.c
+++ christy40/christy40/keymaps/default/keymap.c
@@ -20,34 +20,25 @@
 #define G_PSCR      LGUI(KC_PSCR)
 #define CA_DEL      LCTL(LALT(KC_DEL))
 #define A_F4        LALT(KC_F4)
-#define G_LEFT      RGUI(KC_LEFT)
-#define G_PGDN      RGUI(KC_PGDN)
-#define G_UP        RGUI(KC_UP)
-#define G_RGHT      RGUI(KC_RGHT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT(
+    [0] = LAYOUT_(
         KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
         KC_TAB,      KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_BSLS,
         KC_LSFT,       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_LSFT,
                   KC_LCTL,   MO(1),   KC_ENT,             KC_CAPS,   KC_SPC,                  MO(2),   KC_RALT
     ),
-    [1] = LAYOUT(
+    [1] = LAYOUT_(
         KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
         G_TAB,       KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_DQUO,
-        _______,       KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  G_PSCR,  CA_DEL,  KC_HOME, KC_END,  _______,
+        _______,       KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  G_PSCR,  CA_DEL,  RGB_VAI, RGB_VAD,  _______,
                   _______,   _______, _______,            _______,   _______,                 _______, _______
     ),
-    [2] = LAYOUT(
+    [2] = LAYOUT_(
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-        KC_DEL,      A_F4,    KC_UP,   G_LEFT,  G_PGDN,  G_UP,    G_RGHT,  KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_QUOT,
-        RESET,         KC_LEFT, KC_DOWN, KC_RGHT, KC_VOLD, KC_VOLU, KC_MPRV, KC_MPLY, KC_MNXT, KC_PGUP, KC_PGDN, _______,
+        KC_DEL,      A_F4,    KC_UP,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_QUOT,
+        RESET,         KC_LEFT, KC_DOWN, KC_RGHT, KC_VOLD, KC_VOLU, KC_MPRV, KC_MPLY, KC_MNXT, RGB_SAI, RGB_SAD, _______,
                   _______,   _______, _______,            _______,   _______,                 _______, _______
     ),
-    [3] = LAYOUT(
-        _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,
-        _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,
-        _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,
-                  _______,   _______, _______,            _______,   _______,                 _______, _______
-    ),
+
 };
